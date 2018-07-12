96f5e1c02114326217667c927b20a321af00e24c
chetan <chetan882777@gmail.com>
2018-07-12 15:36:34 +0530

    Added branch view with clickes

--- app/src/main/java/com/heptware/android/aaijihostel/MainActivity.java
+++ app/src/main/java/com/heptware/android/aaijihostel/MainActivity.java
@@ -1,5 +1,6 @@
 package com.heptware.android.aaijihostel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private String recievedString;
+    // used for geting string sent from branchView activity.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        // get intent sent from branchView avtivity
+        // and fetch string from it for listing pourpose.
+        recievedString = intent.getStringExtra("students");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
